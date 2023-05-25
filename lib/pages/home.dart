b94e34070037d60ed2ecc3d9276be9480b019111
Jonas Rasmussen <jbr@duka.dk>
2023-05-25 10:10:07 +0200

    Tilbage til listView

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -59,7 +59,7 @@ class Home extends StatelessWidget {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (context) => OrdForklaringer()),
+                          builder: (context) => const OrdForklaringer()),
                     );
                   },
                 ),
